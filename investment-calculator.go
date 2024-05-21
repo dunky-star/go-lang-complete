ff59756413b92abd46125296662f56bfad4986b2
dunky-star <dunkygeoffrey39@gmail.com>
2024-05-21 01:07:34 -0500

    Go Types and Null values

--- investment-calculator.go
+++ investment-calculator.go
@@ -7,10 +7,21 @@ import (
 
 func main(){
 
-	var investmentAmount float64 = 1000 // Assigning type explicitly to override inferred value
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	const inflationRate = 2.5 // Can not be reassigned
+	// Assigning type explicitly to override inferred value
+	var investmentAmount float64 = 0.0
+	expectedReturnRate := 5.5 
+	years :=   10.0
 
-	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
-    fmt.Print(`The investment return is: `, futureValue)
+	fmt.Print("Enter an investment amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Enter the number of years to invest: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	futureRealValue := investmentAmount * math.Pow(1 + inflationRate/100, years)
+    fmt.Print(`The investment return is: `, futureValue, "\n")
+	fmt.Print(`The real value after inflation: `, futureRealValue)
 }
