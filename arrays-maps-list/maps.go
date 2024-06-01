01ad316cd92f28c510992ace9db6ab77452d8fc1
dunky-star <dunkygeoffrey39@gmail.com>
2024-06-01 11:43:43 -0500

    Special make function for Arrays and Maps

--- arrays-maps-list/maps.go
+++ arrays-maps-list/maps.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	websites := map[string]string{
-		"Google": "https://google.com",
-		"Amazon": "https://aws.com",
+		"Google":  "https://google.com",
+		"Amazon":  "https://aws.com",
+		"Twitter": "https://x.com",
+		"IBM":     "https://ibm.com",
 	}
 	websites["LinkedIn"] = "https://linkedin.com" // Adding an element to a map
+	websites["Sony"] = "https://sony.com"
+	websites["Huawei"] = "https://huawei.com"
+	websites["ubs"] = "https://ubs.com"
 	fmt.Println(websites)
 
 	courseRatings := make(map[string]float64, 3)
@@ -16,6 +21,13 @@ func main() {
 	courseRatings["Java"] = 4.8
 	courseRatings["Angular"] = 4.7
 
-	fmt.Print(courseRatings)
+	fmt.Println(courseRatings)
+
+	// Using For Loop with Arrays, Slices and Maps
+	for i, val := range websites {
+		//...
+		fmt.Println(i)
+		fmt.Println(val)
+	}
 
 }
